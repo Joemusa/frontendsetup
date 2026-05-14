f50585133c3bc8f2f09c64cbfc8b77f04a99f45d
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-05-14 15:02:53 +0200

    Comment out GCP service account write line
    Comment out the line that writes GCP service account to the stream.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.set_page_config(
     page_icon="📊",
     layout="wide"
 )
-st.write(st.secrets["gcp_service_account"]["frontendsetup@rentabuka.iam.gserviceaccount.com"])
+#st.write(st.secrets["gcp_service_account"]["frontendsetup@rentabuka.iam.gserviceaccount.com"])
 
 
 # =========================================================
